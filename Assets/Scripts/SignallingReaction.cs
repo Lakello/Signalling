1b75d719e18d95b3aa4451c6eaffcd8aab8c5f43
Lakello <59009343+Lakello@users.noreply.github.com>
2022-10-18 21:07:04 +0700

    Update SignallingReaction.cs

--- Assets/Scripts/SignallingReaction.cs
+++ Assets/Scripts/SignallingReaction.cs
@@ -7,37 +7,33 @@ using static UnityEngine.GraphicsBuffer;
 public class SignallingReaction : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
-    [SerializeField] [Range(0.01f, 2f)] private float _multiplierDelta;
+    [SerializeField][Range(0.01f, 2f)] private float _multiplierDelta;
     [SerializeField] private UnityEvent _onPlayAudio;
     [SerializeField] private UnityEvent _onStopAudio;
 
     private float _maxVolume = 1f;
     private float _minVolume = 0f;
     private bool _isInvasion;
-    private bool _isSuccess;
     private Coroutine _changeVolumeSmoothlyCoroutine;
 
     private IEnumerator ChangeVolumeSmoothly(float target)
     {
-        while (_isSuccess == false)
+        while (_audioSource.volume != target)
         {
             _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, target, _multiplierDelta * Time.deltaTime);
 
-            if (_audioSource.volume == target)
-            {
-                _isSuccess = true;
-
-                if (_isInvasion == false) _onStopAudio?.Invoke();
-            }
-
             yield return null;
+        }
+
+        if (_isInvasion == false)
+        {
+            _onStopAudio?.Invoke();
         }
     }
 
     public void OnInvasion(bool isInvasion)
     {
         _isInvasion = isInvasion;
-        _isSuccess = false;
 
         if (_isInvasion)
         {
