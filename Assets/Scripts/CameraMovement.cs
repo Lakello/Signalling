e607821618a5bd513d7727db3573f648743ada81
Lakello <59009343+Lakello@users.noreply.github.com>
2022-10-19 12:41:14 +0700

    g

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCamera : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Transform _camera;
 
